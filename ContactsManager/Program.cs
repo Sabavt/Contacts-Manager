a51eada5538e4f7c8c07d92398013acd5f294e97
Sabavt <envecta.info@proton.me>
2026-09-05 18:07:05 +0400

    Configure DbContext to use SQL Server from appsettings.json
    Updated DbContext to read SQL Server connection string from appsettings.json via builder.Configuration["ConnectionStrings:DefaultConnection"]. Added "ConnectionStrings" section with "DefaultConnection" entry to appsettings.json for local SQL Server setup.

--- ContactsManager/Program.cs
+++ ContactsManager/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IPersonsService, PersonsService>();
 builder.Services.AddSingleton<ICountriesService, CountriesService>();
-builder.Services.AddDbContext<PersonsDbContext>(options => options.UseSqlServer());
+builder.Services.AddDbContext<PersonsDbContext>(options => options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]));
 var app = builder.Build(); 
 
 if (builder.Environment.IsDevelopment())
