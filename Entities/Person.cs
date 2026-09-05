8d0431c72e20796f7151465c481913a463652d2c
Sabavt <envecta.info@proton.me>
2026-09-05 17:25:53 +0400

    Add EF Core annotations and DbContext for SQL Server
    Added data annotations to Person and Country for EF Core validation and schema definition. Added Microsoft.EntityFrameworkCore.SqlServer package to Entities.csproj. Introduced PersonsDbContext with DbSet properties for Person and Country.

--- Entities/Person.cs
+++ Entities/Person.cs
@@ -1,15 +1,24 @@
-﻿namespace Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities;
 
 /// <summary>
 /// Domain Model for Person
 /// </summary>
 public class Person
 {
-    public Guid PersonID { get; set; } 
+    [Key]
+    public Guid PersonID { get; set; }
+
+    [StringLength(40)]
     public string? PersonName { get; set; } 
+
+    [StringLength(40)] 
     public string? Email { get; set; }
-    public DateTime? DateOfBirth { get; set; }
-    public string? Gender { get; set; } = null!;
+    public DateTime? DateOfBirth { get; set; } 
+
+    [StringLength(8)] 
+    public string? Gender { get; set; } = null!; 
     public Guid? CountryID { get; set; }
     public string? Address { get; set; }
     public bool? ReceiveNewsLetters { get; set; }
---
+++ Entities/PersonsDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Entities;
+
+public class PersonsDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Country> Countries { get; set; }
+}
