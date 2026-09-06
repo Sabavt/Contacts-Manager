5d175e848324e191132cefdb67a38b2781dc51a2
Sabavt <envecta.info@proton.me>
2026-09-06 17:37:33 +0400

    Add migration for usp_GetAllPersons stored procedure
    Created EF Core migration to add usp_GetAllPersons procedure for selecting all records from Persons table. Updated model snapshot and included seed data for Country and Person entities. Noted incorrect DROP PROCEDURE syntax in Down method.

---
+++ Entities/Migrations/20260906131633_usp_GetPersons.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class usp_GetPersons : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string usp_GetAllPersons = @"
+                CREATE PROCEDURE usp_GetAllPersons
+                AS
+                BEGIN
+                    SELECT * FROM Persons
+                END";
+            migrationBuilder.Sql(usp_GetAllPersons);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string usp_GetAllPersons = @"
+                DROP PROCEDURE usp_GetAllPersons
+                AS
+                BEGIN
+                    SELECT * FROM Persons
+                END";
+            migrationBuilder.Sql(usp_GetAllPersons);
+        }
+    }
+}
