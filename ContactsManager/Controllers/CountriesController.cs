ff718759c71cdc2a1735adaf915d352cdd86f4cf
Sabavt <envecta.info@proton.me>
2026-09-08 15:44:22 +0400

    Add Excel upload for countries and new controller
    Added UploadCountriesFromExcel to PersonsService to import countries from Excel and add new ones to the database. Introduced CountriesController with Index action and routing.

---
+++ ContactsManager/Controllers/CountriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManager.Controllers
+{
+    [Route("[controller]")]
+    public class CountriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
