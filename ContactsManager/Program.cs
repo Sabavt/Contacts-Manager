81fce7271dfb8caa95eb11a91d054f0115241ae2
Sabavt <envecta.info@proton.me>
2026-09-09 16:45:46 +0400

    Add Repositories project and configure dependencies
    Added Repositories project to solution and referenced it in ContactsManager.slnx. Implemented CountriesRepository class with method stubs for ICountriesRepository. Registered dependencies on Entities and RepositoryContracts in Repositories.csproj. Updated Program.cs to configure Rotativa and refactored AddDbContext for clarity.

--- ContactsManager/Program.cs
+++ ContactsManager/Program.cs
@@ -5,23 +5,24 @@ using ServiceContracts;
 using Services;
 
 ExcelPackage.License.SetNonCommercialPersonal("Saba-Contacts-Manager");
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews(); 
+Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();  
 builder.Services.AddScoped<IPersonsService, PersonsService>();
 builder.Services.AddScoped<ICountriesService, CountriesService>();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options
-.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build(); 
 
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-}
-
-Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
-
+} 
 
 app.UseStaticFiles();
 app.MapControllers();
---
+++ Repositories/CountriesRepository.cs
@@ -0,0 +1,34 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace Repositories;
+
+public class CountriesRepository : ICountriesRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public CountriesRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public Task<Country> AddCountry(Country country)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Country>> GetAllCountries()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Country?> GetCountryByCountryID(Guid? countryID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Country?> GetCountryByCountryName(string? countryName)
+    {
+        throw new NotImplementedException();
+    }
+}
