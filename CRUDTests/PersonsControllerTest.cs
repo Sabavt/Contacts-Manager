ba955e1f3f8303a67a5df8c847d66390b007fafa
Sabavt <envecta.info@proton.me>
2026-09-10 20:40:00 +0400

    Add unit test for PersonsController Index action
    Added test in PersonsControllerTest to verify Index returns a ViewResult with an IEnumerable<PersonResponse> model matching expected data. Used Moq for service setup and FluentAssertions for assertions.

--- CRUDTests/PersonsControllerTest.cs
+++ CRUDTests/PersonsControllerTest.cs
@@ -5,6 +5,7 @@ using FluentAssertions;
 using ContactsManager.Controllers;
 using ServiceContracts.DTO;
 using ServiceContracts.Enums;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CRUDTests;
 
@@ -33,7 +34,17 @@ public class PersonsControllerTest
         PersonsController personsController = new PersonsController(_countriesService, _personsService);
 
         _personsServiceMock.Setup(t => t.GetFilteredPersons(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(new List<PersonResponse>());
-        _personsServiceMock.Setup(t => t.GetSortedPerson(It.IsAny<List<PersonResponse>>(), It.IsAny<string>(), It.IsAny<SortOrderOptions>())).ReturnsAsync(new List<PersonResponse>()); 
+        _personsServiceMock.Setup(t => t.GetSortedPerson(It.IsAny<List<PersonResponse>>(), It.IsAny<string>(), It.IsAny<SortOrderOptions>())).ReturnsAsync(new List<PersonResponse>());
+
+       IActionResult result = await personsController.Index(
+           _fixture.Create<string>(), _fixture.Create<string>(),
+           _fixture.Create<string>(), _fixture.Create<SortOrderOptions>()
+       );
+
+       ViewResult view = Assert.IsType<ViewResult>(result);
+
+        view.ViewData.Model.Should().BeAssignableTo<IEnumerable<PersonResponse>>();
+        view.ViewData.Model.Should().Be(persons_response_list);
     }
-        
+
 }
