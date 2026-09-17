806b641f80d7d38194b6b9b7e0f8056a80803ce1
Sabavt <envecta.info@proton.me>
2026-09-17 13:48:32 +0400

    Add DTO import and attribute routing to AccountController
    Introduced Register GET and POST actions using RegisterRequest DTO.
    POST action redirects to Persons/Index after registration.

--- ContactsManager.UI/Controllers/AccountController.cs
+++ ContactsManager.UI/Controllers/AccountController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContactsManager.Core.DTO;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ContactsManager.UI.Controllers;
 
+[Route("[controller]")]
 public class AccountController : Controller
 {
+    [HttpGet]
+    [Route("[action]")]
     public IActionResult Register()
     {
         return View();
+    }
+
+    [HttpPost]
+    [Route("[action]")]
+    public IActionResult Register(RegisterRequest registerRequest)
+    {
+        return RedirectToActionPermanent("Index", "Persons"); 
     }
 }
