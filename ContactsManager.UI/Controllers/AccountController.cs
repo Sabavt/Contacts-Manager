31467e2e2ad68183ce700c520b6913da1b0e007a
Sabavt <envecta.info@proton.me>
2026-09-17 13:24:11 +0400

    Add AccountController and registration view
    Added AccountController with Register action. Created Register.cshtml Razor view using RegisterRequest model. Updated _ViewImports.cshtml to include ContactsManager.Core.DTO namespace.

---
+++ ContactsManager.UI/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManager.UI.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
