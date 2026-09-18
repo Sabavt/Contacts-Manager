4e56d4497574a96b60641a65778d7131b094d746
Sabavt <envecta.info@proton.me>
2026-09-18 12:08:52 +0400

    Add LogOut action, fix nav links, add LoginRequest DTO
    Introduce empty LogOut action in AccountController. Update _Layout.cshtml to use correct ViewBag.CurrentUrl values for active navigation highlighting. Fix "Upload Countries" link to use UploadExcel action. Add LoginRequest DTO with Email and Password validation.

---
+++ ContactsManager.Core/DTO/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsManager.Core.DTO;
+
+public class LoginRequest
+{
+    [Required]
+    [EmailAddress]
+    [DataType(DataType.EmailAddress)]
+    public string? Email { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string? Password { get; set; }
+}
--- ContactsManager.UI/Controllers/AccountController.cs
+++ ContactsManager.UI/Controllers/AccountController.cs
@@ -61,6 +61,11 @@ public class AccountController : Controller
             return View(registerRequest);
         }
     }
+     
+    public async Task<IActionResult> LogOut()
+    {
+
+    }
 
 
 }
